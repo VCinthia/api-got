bb0d693b2db624ddc9f8d24067d753f0100eb3f2
cinth <votacinthia@gmail.com>
2024-03-14 21:44:03 -0300

    First commit - structure front & back

---
+++ backend/src/modules/example/example.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { CreateExampleDto } from './dto/create-example.dto';
+import { UpdateExampleDto } from './dto/update-example.dto';
+
+@Injectable()
+export class ExampleService {
+  create(createExampleDto: CreateExampleDto) {
+    return 'This action adds a new example';
+  }
+
+  findAll() {
+    return `This action returns all example`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} example`;
+  }
+
+  update(id: number, updateExampleDto: UpdateExampleDto) {
+    return `This action updates a #${id} example`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} example`;
+  }
+}
